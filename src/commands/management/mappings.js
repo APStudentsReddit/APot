143d1cd70eab9c409476d40bbfed618b954c8f79
Unknown <qxest@reqxest.com>
2018-07-13 20:23:28 -0400

    Create mappings

---
+++ src/commands/management/mappings.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord-akairo')
+const redis = require('../../structures/database.js')
+
+class MappingsCommand extends Command {
+  constructor () {
+    super('map', {
+      aliases: 'm',
+      category: 'management',
+      args: [],
+      userPermissions: ['MANAGE_ROLES'],
+      channelRestriction: 'guild',
+      description: {
+        content: 'Lists mappings of aliases'
+      }
+    })
+  }
+
+  async exec (message, args) {
+    const status = await message.reply(`Fetching mappings...`)
+
+    const mappings = await redis.db.hgetallAsync(`${message.guild.id}.aliasMappings`)
+    let blacklist = await redis.db.hgetAsync(message.guild.id, 'blacklist')
+    blacklist = JSON.parse(blacklist)
+    status.edit('aliases: ' + JSON.stringify(mappings))
+    message.channel.send('blacklist: ' + blacklist.join(', '))
+  }
+}
+
+module.exports = MappingsCommand
