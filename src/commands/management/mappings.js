ffb85b96258ab7a8e05a558aead8d4fdb81fb27d
suufi <qxest@reqxest.com>
2018-07-16 21:43:48 +0000

    Show all roles and aliases regardless of how many there are

--- src/commands/management/mappings.js
+++ src/commands/management/mappings.js
@@ -17,21 +17,45 @@ class MappingsCommand extends Command {
   async exec (message, args) {
     const status = await message.reply(`Fetching mappings...`)
 
-    const mappings = await this.client.settings.get(message.guild.id, 'aliasMappings', {})
     const blacklist = await this.client.settings.get(message.guild.id, 'blacklist', [])
+
+    const helperRoles = await message.guild.roles.filter(role => role.name.toLowerCase().includes('helper')).sort((a, b) => { if (a.name < b.name) return -1; if (a.name > b.name) return 1; return 0 })
 
     let aliases = []
 
-    for (var alias in mappings) {
-      aliases.push(`${alias} : <@&${mappings[alias]}>`)
-    }
+    await helperRoles.map(async (role) => {
+      const dbRole = await this.client.settings.get(message.guild.id, 'roles', {})[role.id] || {}
+      let roleAliases
+      if (dbRole.aliases) {
+        roleAliases = dbRole.aliases.join(', ')
+      } else {
+        roleAliases = 'none'
+      }
+      aliases.push(`<@&${role.id}> : ${roleAliases}`)
+      if (aliases.join('\n').length >= 2000) {
+        aliases = aliases.slice(0, -1)
+        message.channel.send(aliases.join('\n'))
+        aliases = []
+        aliases.push(`<@&${role.id}> : ${roleAliases}`)
+      }
+    })
 
-    const embed = await this.client.util.embed()
-      .addField('Prefix', await this.client.settings.get(message.guild.id, 'prefix', '>'))
-      .addField('Blacklist', blacklist.length > 0 ? blacklist.map((id) => `<@${id}>`) : 'none')
-      .addField('Aliases', aliases.length > 0 ? aliases.join('\n') : 'none')
-      .setFooter(`requested by ${message.author.username}`, message.author.displayAvatarURL)
-    await status.edit({ embed })
+    if (aliases.length > 0) {
+      message.channel.send(aliases)
+
+      const embed = await this.client.util.embed()
+        .addField('Prefix', await this.client.settings.get(message.guild.id, 'prefix', '>'))
+        .addField('Blacklist', blacklist.length > 0 ? blacklist.map((id) => `<@${id}>`) : 'none')
+        .setFooter(`requested by ${message.author.username}`, message.author.displayAvatarURL)
+      await status.edit({ embed })
+    } else {
+      const embed = await this.client.util.embed()
+        .addField('Prefix', await this.client.settings.get(message.guild.id, 'prefix', '>'))
+        .addField('Blacklist', blacklist.length > 0 ? blacklist.map((id) => `<@${id}>`) : 'none')
+        .addField('Aliases: ', aliases)
+        .setFooter(`requested by ${message.author.username}`, message.author.displayAvatarURL)
+      await status.edit({ embed })
+    }
   }
 }
 
